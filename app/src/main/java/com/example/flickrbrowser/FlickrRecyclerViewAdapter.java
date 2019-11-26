27981cf06a83c675a888a7607aaec0a01e9e2c0e
Jamie Randall <bentleyx007@gmail.com>
2019-11-26 13:21:59 -0800

    initial commit

---
+++ app/src/main/java/com/example/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package com.example.flickrbrowser;
+
+import android.content.Context;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder> {
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotosList;
+    private Context mContext;
+
+    public FlickrRecyclerViewAdapter(List<Photo> photosList, Context context) {
+        mPhotosList = photosList;
+        mContext = context;
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Called by the layoutmanager when it needs a new view
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    void loadNewData(List<Photo> newPhotos) {
+        mPhotosList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position) {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.get(position) : null);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+
+        //Called by layout manager when it wants new data in an existing row
+
+        Photo photoItem = mPhotosList.get(position);
+        Log.d(TAG, "onBindViewHolder: " + photoItem.getTitle() + "--->" + position);
+        Picasso.with(mContext).load(photoItem.getImage())
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail);
+
+        holder.title.setText(photoItem.getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ((mPhotosList != null) && (mPhotosList.size() != 0) ? mPhotosList.size() : 0);
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView) itemView.findViewById(R.id.title);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/flickrbrowser/PhotoDetailActivity.java
@@ -0,0 +1,23 @@
+package com.example.flickrbrowser;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class PhotoDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+
+        activateToolbar(true);
+    }
+
+}
